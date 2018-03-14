6a497f295140d3536da9b39893e1fa736aab01d6
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2018-03-13 21:41:24 -0400

    continuous-time Markov model

--- CompareTherapies.py
+++ CompareTherapies.py
@@ -1,8 +1,7 @@
 import ParameterClasses as P
 import MarkovModelClasses as MarkovCls
 import SupportMarkovModel as SupportMarkov
-import scr.SamplePathClasses as PathCls
-import scr.FigureSupport as Figs
+
 
 # simulating mono therapy
 # create a cohort
@@ -22,38 +21,9 @@ cohort_combo = MarkovCls.Cohort(
 # simulate the cohort
 simOutputs_combo = cohort_combo.simulate()
 
-# get survival curves of both treatments
-survival_curves = [
-    simOutputs_mono.get_survival_curve(),
-    simOutputs_combo.get_survival_curve()
-    ]
 
-# graph survival curve
-PathCls.graph_sample_paths(
-    sample_paths=survival_curves,
-    title='Survival curve',
-    x_label='Simulation time step',
-    y_label='Number of alive patients',
-    legends=['Mono Therapy', 'Combination Therapy']
-    )
-
-# histograms of survival times
-set_of_survival_times = [
-    simOutputs_mono.get_survival_times(),
-    simOutputs_combo.get_survival_times()
-]
-
-# graph histograms
-Figs.graph_histograms(
-    data_sets=set_of_survival_times,
-    title='Histogram of patient survival time',
-    x_label='Survival time',
-    y_label='Counts',
-    bin_width=1,
-    legend=['Mono Therapy', 'Combination Therapy'],
-    transparency=0.6
-)
-
+# draw survival curves and histograms
+SupportMarkov.draw_figures(simOutputs_mono, simOutputs_combo)
 
 # print the estimates for the mean survival time and mean time to AIDS
 SupportMarkov.print_outcomes(simOutputs_mono, "Mono Therapy:")
--- SupportMarkovModel.py
+++ SupportMarkovModel.py
@@ -1,5 +1,7 @@
-import scr.FormatFunctions as Format
 import InputData as Params
+import scr.FormatFunctions as Format
+import scr.SamplePathClasses as PathCls
+import scr.FigureSupport as Figs
 
 
 def print_outcomes(simOutput, therapy_name):
@@ -38,3 +40,38 @@ def print_outcomes(simOutput, therapy_name):
     print("  Estimate of discounted utility and {:.{prec}%} confidence interval:".format(1 - Params.ALPHA, prec=0),
           utility_mean_CI_text)
     print("")
+
+
+def draw_figures(simOutputs_mono, simOutputs_combo):
+
+    # get survival curves of both treatments
+    survival_curves = [
+        simOutputs_mono.get_survival_curve(),
+        simOutputs_combo.get_survival_curve()
+    ]
+
+    # graph survival curve
+    PathCls.graph_sample_paths(
+        sample_paths=survival_curves,
+        title='Survival curve',
+        x_label='Simulation time step',
+        y_label='Number of alive patients',
+        legends=['Mono Therapy', 'Combination Therapy']
+    )
+
+    # histograms of survival times
+    set_of_survival_times = [
+        simOutputs_mono.get_survival_times(),
+        simOutputs_combo.get_survival_times()
+    ]
+
+    # graph histograms
+    Figs.graph_histograms(
+        data_sets=set_of_survival_times,
+        title='Histogram of patient survival time',
+        x_label='Survival time',
+        y_label='Counts',
+        bin_width=1,
+        legend=['Mono Therapy', 'Combination Therapy'],
+        transparency=0.6
+    )
